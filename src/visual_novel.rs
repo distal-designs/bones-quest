f6e53d1a43416a140a8f01253346277768f89b2f
Nicholas Farley <nicholasafarley@gmail.com>
2017-10-30 01:33:07 -0400

    Add Deserialize, Debug derive statements to VN types

--- src/visual_novel.rs
+++ src/visual_novel.rs
@@ -2,16 +2,21 @@ extern crate toml;
 
 
 
+#[derive(Deserialize, Debug)]
 pub enum BackgroundCommand {
     Hide,
     Show(String),
 }
 
+
+#[derive(Deserialize, Debug)]
 pub enum PortraitCommand {
     Hide,
     Show(String, String)
 }
 
+
+#[derive(Deserialize, Debug)]
 pub struct Command {
     background: Option<BackgroundCommand>,
     portrait: Option<PortraitCommand>,
