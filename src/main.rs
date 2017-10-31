53b63029922e223e2bcb61f47167fef63de8ded3
Nicholas Farley <nicholasafarley@gmail.com>
2017-10-30 22:43:56 -0400

    Change test in main to load "blood" dialog file

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,5 @@ extern crate serde_derive;
 mod visual_novel;
 
 fn main() {
-    let dialog = visual_novel::Command::parse(r#"
-    [[command]]
-    text = "this is some text"
-
-    [[command]]
-    text = "so is this"
-    "#);
-    println!("{:?}", dialog);
+    println!("{:?}", visual_novel::Command::load("blood"));
 }
