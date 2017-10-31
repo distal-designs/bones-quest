83cba0d31506239d3e1208a991c94a8d34b81536
Nicholas Farley <nicholasafarley@gmail.com>
2017-10-30 22:43:20 -0400

    Add visual_novel::Command::load to load dialog files

--- src/visual_novel.rs
+++ src/visual_novel.rs
@@ -1,6 +1,11 @@
 extern crate toml;
 
 
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+
 
 #[derive(Deserialize, Debug)]
 pub enum BackgroundCommand {
@@ -33,5 +38,21 @@ impl Command {
         }
 
         toml::from_str::<Commands>(toml).map(|commands| commands.command)
+    }
+
+
+    pub fn load (dialog_name: &str) -> Result<Vec<Command>, toml::de::Error> {
+        let mut pathbuf = env::current_dir().unwrap();
+        pathbuf.push("resources");
+        pathbuf.push("dialogs");
+        pathbuf.push(format!("{}.toml", dialog_name));
+        let path = pathbuf.as_path();
+        println!("{:?}", path);
+        let mut f = File::open(path).unwrap();
+        let mut toml = String::new();
+        f.read_to_string(&mut toml)
+         .unwrap();
+
+        Command::parse(&toml)
     }
 }
