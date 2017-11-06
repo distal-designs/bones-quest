fad170969b64a660a5f49fdcfc27ec0d4d53611a
Nicholas Farley <nicholasafarley@gmail.com>
2017-11-05 23:28:01 -0500

    Define (basic) EventHandler trait for MainState

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate serde_derive;
 extern crate ggez;
 
 use ggez::{conf, Context, GameResult, event};
+use ggez::graphics;
+use std::time::Duration;
 
 mod visual_novel;
 
@@ -14,6 +16,19 @@ struct MainState {
 impl MainState {
     fn new(_: &mut Context) -> GameResult<MainState>  {
         Ok(MainState {})
+    }
+}
+
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _: &mut Context, _: Duration) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        graphics::present(ctx);
+        Ok(())
     }
 }
 
