3d59e428193038866a56c1ddd6e0b2ed5f6a20e6
Nicholas Farley <nicholasafarley@gmail.com>
2017-11-05 23:27:44 -0500

    Define basic MainState::new()

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 #[macro_use]
 extern crate serde_derive;
+extern crate ggez;
+
+use ggez::{conf, Context, GameResult, event};
 
 mod visual_novel;
 
 
 struct MainState {
+}
+
+
+impl MainState {
+    fn new(_: &mut Context) -> GameResult<MainState>  {
+        Ok(MainState {})
+    }
 }
 
 
