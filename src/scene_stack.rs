b3036c89658aff3fb4ad3393f0babc370a91090e
Nicholas Farley <nicholasafarley@gmail.com>
2017-11-25 20:18:31 -0500

    Add SceneStack implementation for Vec<Box<Scene<W>>>

--- src/scene_stack.rs
+++ src/scene_stack.rs
@@ -7,3 +7,27 @@ pub trait SceneStack<W> {
     fn update(&mut self, world: &mut W);
     fn draw(&self, world: &W, ctx: &mut ggez::Context);
 }
+
+impl<W> SceneStack<W> for Vec<Box<Scene<W>>> {
+    fn pop(&mut self) -> Option<Box<Scene<W>>> {
+        self.pop()
+    }
+
+    fn push (&mut self, scene: Box<Scene<W>>) {
+        self.push(scene);
+    }
+
+    fn update(&mut self, world: &mut W) {
+        let current_index = self.len() - 1;
+        if let Some(scene) = self.get_mut(current_index) {
+            scene.update(world);
+        }
+    }
+
+    fn draw(&self, world: &W, ctx: &mut ggez::Context) {
+        let current_index = self.len() - 1;
+        if let Some(scene) = self.get(current_index) {
+            scene.draw(world, ctx);
+        }
+    }
+}
