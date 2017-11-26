720f2b3dcbb4cdae746765a6acf00ce14c866e45
Nicholas Farley <nicholasafarley@gmail.com>
2017-11-25 21:24:11 -0500

    Move MainState definitions to separate file

--- src/main.rs
+++ src/main.rs
@@ -2,72 +2,15 @@
 extern crate serde_derive;
 extern crate ggez;
 
-use ggez::{conf, Context, GameResult, event};
-use ggez::graphics;
-use ggez::event::{Keycode, Mod};
-use ggez::graphics::{Text, Font, Drawable, Point};
-use std::time::Duration;
+use ggez::{conf, Context, event};
 
 use visual_novel::command::Command;
+use main_state::MainState;
 
 mod visual_novel;
 mod scene_stack;
 mod scene;
-
-
-struct MainState {
-    dialog: Vec<Command>,
-    font: Font,
-    dialog_index: usize,
-}
-
-
-impl MainState {
-    fn new(_: &mut Context, dialog: Vec<Command>) -> GameResult<MainState> {
-        Ok(MainState {
-            dialog,
-            font:  Font::default_font().unwrap(),
-            dialog_index: 0,
-        })
-    }
-}
-
-
-impl event::EventHandler for MainState {
-    fn update(&mut self, _: &mut Context, _: Duration) -> GameResult<()> {
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
-        let (_, lines) = self.font.get_wrap(&self.dialog[self.dialog_index].text, 700);
-
-        for (index, line) in lines.iter().enumerate() {
-            Text::new(ctx, &line, &self.font)
-                .unwrap()
-                .draw(ctx, Point { x: 400.0, y: index as f32 * 25.0 + 100.0 }, 0.0)
-                .unwrap();
-        }
-
-        graphics::present(ctx);
-        Ok(())
-    }
-
-    fn key_up_event(&mut self, keycode: Keycode, _: Mod, _: bool) {
-        match keycode {
-            Keycode::Left => self.dialog_index = match self.dialog_index {
-                0 => 0,
-                x => x - 1,
-            },
-            Keycode::Right => self.dialog_index = match self.dialog_index {
-                x if x == self.dialog.len() - 1 => x,
-                x => x + 1,
-            },
-            _ => {},
-        };
-    }
-}
-
+mod main_state;
 
 fn main() {
     let mut c = conf::Conf::default();
---
+++ src/main_state.rs
@@ -0,0 +1,60 @@
+use ggez::{Context, GameResult, event};
+use ggez::graphics;
+use ggez::event::{Keycode, Mod};
+use ggez::graphics::{Text, Font, Drawable, Point};
+use std::time::Duration;
+
+use visual_novel::command::Command;
+
+pub struct MainState {
+    dialog: Vec<Command>,
+    font: Font,
+    dialog_index: usize,
+}
+
+
+impl MainState {
+    pub fn new(_: &mut Context, dialog: Vec<Command>) -> GameResult<MainState> {
+        Ok(MainState {
+            dialog,
+            font:  Font::default_font().unwrap(),
+            dialog_index: 0,
+        })
+    }
+}
+
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _: &mut Context, _: Duration) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        let (_, lines) = self.font.get_wrap(&self.dialog[self.dialog_index].text, 700);
+
+        for (index, line) in lines.iter().enumerate() {
+            Text::new(ctx, &line, &self.font)
+                .unwrap()
+                .draw(ctx, Point { x: 400.0, y: index as f32 * 25.0 + 100.0 }, 0.0)
+                .unwrap();
+        }
+
+        graphics::present(ctx);
+        Ok(())
+    }
+
+    fn key_up_event(&mut self, keycode: Keycode, _: Mod, _: bool) {
+        match keycode {
+            Keycode::Left => self.dialog_index = match self.dialog_index {
+                0 => 0,
+                x => x - 1,
+            },
+            Keycode::Right => self.dialog_index = match self.dialog_index {
+                x if x == self.dialog.len() - 1 => x,
+                x => x + 1,
+            },
+            _ => {},
+        };
+    }
+}
