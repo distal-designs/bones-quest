c7b1fbe98ec9fa3f99b12774ea1ef74e92d79465
Nicholas Farley <nicholasafarley@gmail.com>
2017-11-25 20:14:22 -0500

    Change Scene::draw to take generic mutably

--- src/scene.rs
+++ src/scene.rs
@@ -2,5 +2,5 @@ use ggez;
 
 pub trait Scene<State> {
     fn update(&mut self, state: &mut State);
-    fn draw(&mut self, state: &mut State, ctx: &mut ggez::Context);
+    fn draw(&mut self, state: State, ctx: &mut ggez::Context);
 }
