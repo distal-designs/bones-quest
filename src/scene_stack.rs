bacb439f99f4b886cda0eec31551c075a5922189
Nicholas Farley <nicholasafarley@gmail.com>
2017-12-08 20:38:44 -0500

    Redefine SceneStack trait in terms of new Scene

--- src/scene_stack.rs
+++ src/scene_stack.rs
@@ -1,11 +1,14 @@
-use scene::Scene;
 use ggez;
 
-pub trait SceneStack<W> {
-    fn pop(&mut self) -> Option<Box<Scene<W>>>;
-    fn push (&mut self, scene: Box<Scene<W>>);
-    fn update(&mut self, world: &mut W);
-    fn draw(&self, world: &W, ctx: &mut ggez::Context);
+use scene::Scene;
+use input::Input;
+use flags::Flags;
+
+pub trait SceneStack {
+    fn pop(&mut self) -> Option<Box<Scene>>;
+    fn push (&mut self, scene: Box<Scene>);
+    fn update(&mut self, input: &Input, flags: &mut Flags);
+    fn draw(&self, flags: &Flags, ctx: &mut ggez::Context);
 }
 
 impl<W> SceneStack<W> for Vec<Box<Scene<W>>> {
