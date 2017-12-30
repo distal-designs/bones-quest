4fc4a75616d30e82b9f60a2888f2a9cf566bce05
Nicholas Farley <nicholasafarley@gmail.com>
2017-12-30 00:02:19 -0500

    Add template scene::Scene impl for Scene struct

--- src/visual_novel/scene.rs
+++ src/visual_novel/scene.rs
@@ -1,9 +1,18 @@
 use ggez::graphics::Font;
+use ggez;
 
 use visual_novel::command::Command;
+use scene;
+use input::Input;
+use flags::Flags;
 
 pub struct Scene {
     dialog: Vec<Command>,
     font: Font,
     dialog_index: usize,
+}
+
+impl scene::Scene for Scene {
+    fn update(&mut self, input: &Input, flags: &mut Flags) { unimplemented!() }
+    fn draw(&self, flags: &Flags, ctx: &mut ggez::Context) { unimplemented!() }
 }
