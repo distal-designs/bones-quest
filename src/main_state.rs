75d4f7dd7833d3bd0bb3add5ca3420a9971370a8
Nicholas Farley <nicholasafarley@gmail.com>
2018-02-04 17:18:27 -0500

    Change MainState::draw() to draw from scenes

--- src/main_state.rs
+++ src/main_state.rs
@@ -33,17 +33,7 @@ impl event::EventHandler for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
-        let (_, lines) = self.font.get_wrap(&self.dialog[self.dialog_index].text, 700);
-
-        for (index, line) in lines.iter().enumerate() {
-            Text::new(ctx, &line, &self.font)
-                .unwrap()
-                .draw(ctx, Point2::new(0.0, index as f32 * 25.0 + 100.0 ), 0.0)
-                .unwrap();
-        }
-
-        graphics::present(ctx);
+        self.scenes.draw(&self.flags, ctx);
         Ok(())
     }
 }
