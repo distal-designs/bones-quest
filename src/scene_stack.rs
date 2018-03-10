9d1b027d30359b3ff4afff194ce98dce792c92f7
Nicholas Farley <nicholasafarley@gmail.com>
2018-03-10 13:16:41 -0500

    Change SceneStack to take type params (for Scene<I, F>)

--- src/scene_stack.rs
+++ src/scene_stack.rs
@@ -1,33 +1,31 @@
 use ggez;
 
 use scene::Scene;
-use input::Input;
-use flags::Flags;
 
-pub trait SceneStack {
-    fn pop(&mut self) -> Option<Box<Scene>>;
-    fn push(&mut self, scene: Box<Scene>);
-    fn update(&mut self, input: &Input, flags: &mut Flags);
-    fn draw(&self, flags: &Flags, ctx: &mut ggez::Context);
+pub trait SceneStack<I, F> {
+    fn pop(&mut self) -> Option<Box<Scene<I, F>>>;
+    fn push(&mut self, scene: Box<Scene<I, F>>);
+    fn update(&mut self, input: &I, flags: &mut F);
+    fn draw(&self, flags: &F, ctx: &mut ggez::Context);
 }
 
-impl SceneStack for Vec<Box<Scene>> {
-    fn pop(&mut self) -> Option<Box<Scene>> {
+impl<I, F> SceneStack<I, F> for Vec<Box<Scene<I, F>>> {
+    fn pop(&mut self) -> Option<Box<Scene<I, F>>> {
         self.pop()
     }
 
-    fn push(&mut self, scene: Box<Scene>) {
+    fn push(&mut self, scene: Box<Scene<I, F>>) {
         self.push(scene);
     }
 
-    fn update(&mut self, input: &Input, flags: &mut Flags) {
+    fn update(&mut self, input: &I, flags: &mut F) {
         let current_index = self.len() - 1;
         if let Some(scene) = self.get_mut(current_index) {
             scene.update(input, flags);
         }
     }
 
-    fn draw(&self, flags: &Flags, ctx: &mut ggez::Context) {
+    fn draw(&self, flags: &F, ctx: &mut ggez::Context) {
         let current_index = self.len() - 1;
         if let Some(scene) = self.get(current_index) {
             scene.draw(flags, ctx);
