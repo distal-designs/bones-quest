1b924be4f91ff3601408335cae9e86007f3452d1
Nicholas Farley <nicholasafarley@gmail.com>
2018-03-13 22:46:52 -0400

    Copy definitions used to draw text to ui module

--- src/engine/ui.rs
+++ src/engine/ui.rs
@@ -1,1 +1,27 @@
+use ggez::{Context, GameResult};
+use ggez::graphics::{Drawable, Point2, Text};
 
+pub struct Message {
+    text: String,
+}
+
+impl Message {
+    pub fn new(text: &str) -> Self {
+        Self {
+            text: text.to_string(),
+        }
+    }
+
+    pub fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        let font = ctx.default_font.clone();
+
+        let (_, lines) = ctx.default_font.get_wrap(&self.text, 700);
+
+        for (index, line) in lines.iter().enumerate() {
+            let text = Text::new(ctx, &line, &font)?;
+            text.draw(ctx, Point2::new(400.0, index as f32 * 25.0 + 100.0), 0.0)?
+        }
+
+        Ok(())
+    }
+}
