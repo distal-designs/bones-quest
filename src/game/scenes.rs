39897a2967e04af714d34642d692358b6fe58dad
Nicholas Farley <nicholasafarley@gmail.com>
2018-03-13 22:52:33 -0400

    Change VisualNovel to use engine::ui module

--- src/game/scenes.rs
+++ src/game/scenes.rs
@@ -1,19 +1,22 @@
 use ggez::graphics;
-use ggez::graphics::{Drawable, Point2, Text};
 use ggez::{self, GameResult};
 
 use engine::visual_novel::command::Command;
+use engine::ui::Message;
 use engine;
 
 pub struct VisualNovel {
     dialog: Vec<Command>,
     dialog_index: usize,
+    message: Message,
 }
 
 impl VisualNovel {
     pub fn new(dialog: Vec<Command>) -> Self {
+        let message = Message::new(&dialog[0].text);
         Self {
             dialog,
+            message,
             dialog_index: 0,
         }
     }
@@ -26,16 +29,7 @@ impl<I, F> engine::scene::Scene<I, F> for VisualNovel {
 
     fn draw(&self, _: &F, ctx: &mut ggez::Context) -> GameResult<()> {
         graphics::clear(ctx);
-        let font = ctx.default_font.clone();
-
-        let (_, lines) = ctx.default_font
-            .get_wrap(&self.dialog[self.dialog_index].text, 700);
-
-        for (index, line) in lines.iter().enumerate() {
-            let text = Text::new(ctx, &line, &font)?;
-            text.draw(ctx, Point2::new(400.0, index as f32 * 25.0 + 100.0), 0.0)?
-        }
-
+        self.message.draw(ctx)?;
         graphics::present(ctx);
         Ok(())
     }
