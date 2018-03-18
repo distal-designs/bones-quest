96e69679310acb01aab7265a224b57552cbb185f
Nicholas Farley <nicholasafarley@gmail.com>
2018-03-18 01:07:08 -0400

    Add engine::color::with_color function

--- src/engine/color.rs
+++ src/engine/color.rs
@@ -1,1 +1,13 @@
+use ggez::graphics::{get_color, set_color, Color};
+use ggez::{Context, GameResult};
 
+pub fn with_color<F>(ctx: &mut Context, color: &Color, fun: F) -> GameResult<()>
+where
+    F: FnOnce(&mut Context) -> GameResult<()>,
+{
+    let current_color = get_color(ctx);
+    set_color(ctx, *color)?;
+    fun(ctx);
+    set_color(ctx, current_color)?;
+    Ok(())
+}
