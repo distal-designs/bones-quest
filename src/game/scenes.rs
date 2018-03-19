f7b47c04e3655653487401210f6efef215493b98
Nicholas Farley <nicholasafarley@gmail.com>
2018-03-19 16:58:01 -0400

    Create stub Conquest scene definition

--- src/game/scenes.rs
+++ src/game/scenes.rs
@@ -31,3 +31,15 @@ impl<I, F> engine::scene::Scene<I, F> for VisualNovel {
         Ok(())
     }
 }
+
+pub struct Conquest;
+
+impl<I, F> engine::scene::Scene<I, F> for Conquest {
+    fn update(&mut self, _: &I, _: &mut F) -> GameResult<()> {
+        unimplemented!();
+    }
+
+    fn draw(&self, _: &F, _ctx: &mut ggez::Context) -> GameResult<()> {
+        unimplemented!();
+    }
+}
