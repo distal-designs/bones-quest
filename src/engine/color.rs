fba93b8e93a0edaf0cc4acde4a478564996959dc
Nicholas Farley <nicholasafarley@gmail.com>
2018-04-04 01:55:31 -0400

    Convert from_hex function to use from_str_radix

--- src/engine/color.rs
+++ src/engine/color.rs
@@ -20,8 +20,8 @@ pub fn from_hex(hex: &str) -> Color {
     let captures = re.captures(hex).expect(INVALID_HEX_MESSAGE);
     let capture = captures.get(1).expect(INVALID_HEX_MESSAGE);
     match capture.as_str() {
-        x if x.len() == 6 => Color::from_rgb_u32(x.parse().expect(INVALID_HEX_MESSAGE)),
-        x if x.len() == 8 => Color::from_rgba_u32(x.parse().expect(INVALID_HEX_MESSAGE)),
+        x if x.len() == 6 => Color::from_rgb_u32(u32::from_str_radix(x, 16).expect(INVALID_HEX_MESSAGE)),
+        x if x.len() == 8 => Color::from_rgba_u32(u32::from_str_radix(x, 16).expect(INVALID_HEX_MESSAGE),
         _ => panic!(INVALID_HEX_MESSAGE),
     }
 }
