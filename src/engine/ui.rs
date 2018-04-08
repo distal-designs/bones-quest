720b2f5afc8793a97e047ad8d99a0c6f99516a6b
Nicholas Farley <nicholasafarley@gmail.com>
2018-04-08 16:10:56 -0400

    Remove struct Message

--- src/engine/ui.rs
+++ src/engine/ui.rs
@@ -1,5 +1,3 @@
-use std::cell::RefCell;
-
 use ggez::{Context, GameResult};
 use ggez::graphics::{self, Color, DrawMode, DrawParam, Drawable, Font, Point2, Rect, Text};
 
@@ -73,94 +71,7 @@ impl Drawable for DialogCache {
     }
 }
 
-pub struct Message {
-    text: String,
-    font_cache: RefCell<Option<Font>>,
-    text_cache: RefCell<Option<Vec<Text>>>,
-}
-
-impl Drawable for Message {
-    fn draw_ex(&self, ctx: &mut Context, _param: DrawParam) -> GameResult<()> {
-        let bounds = Message::bounds(ctx);
-
-        let mut font_cache = self.font_cache.borrow_mut();
-        let font = font_cache.get_or_insert_with(|| ctx.default_font.clone());
-
-        let mut text_cache = self.text_cache.borrow_mut();
-        let texts = text_cache.get_or_insert_with(|| {
-            font.get_wrap(&self.text, bounds.w as usize)
-                .1
-                .iter()
-                .map(|line| Text::new(ctx, &line, &font).unwrap())
-                .collect()
-        });
-
-        graphics::Mesh::new_polygon(
-            ctx,
-            DrawMode::Fill,
-            &[
-                Point2::new(0.0, 0.0),
-                Point2::new(0.0, bounds.h),
-                Point2::new(bounds.w, bounds.h),
-                Point2::new(bounds.w, 0.0),
-            ],
-        )?.draw_ex(
-            ctx,
-            DrawParam {
-                dest: Point2::new(bounds.x, bounds.y),
-                color: Some(Color::from_rgb(0, 0, 0)),
-                ..Default::default()
-            },
-        )?;
-
-        for (index, text) in texts.iter().enumerate() {
-            let x = bounds.x;
-            let y = bounds.y as usize + (index * text.height() as usize);
-            text.draw(ctx, Point2::new(x, y as f32), 0.0)?
-        }
-        Ok(())
-    }
-
-    fn set_blend_mode(&mut self, mode: Option<graphics::BlendMode>) {
-        if let &mut Some(ref mut texts) = self.text_cache.get_mut() {
-            for text in texts.iter_mut() {
-                text.set_blend_mode(mode);
-            }
-        }
-    }
-
-    fn get_blend_mode(&self) -> Option<graphics::BlendMode> {
-        if let Some(texts) = self.text_cache.borrow().as_ref() {
-            for text in texts.iter() {
-                return text.get_blend_mode();
-            }
-        }
-        None
-    }
-}
-
 impl Dialog {
-    fn bounds(ctx: &Context) -> Rect {
-        let width = ctx.conf.window_mode.width;
-        let height = ctx.conf.window_mode.height;
-        Rect {
-            w: width as f32 * 0.8,
-            h: height as f32 * 0.2,
-            x: width as f32 * 0.1,
-            y: height as f32 * 0.7,
-        }
-    }
-}
-
-impl Message {
-    pub fn new(text: &str) -> Self {
-        Self {
-            text: text.to_string(),
-            font_cache: RefCell::new(None),
-            text_cache: RefCell::new(None),
-        }
-    }
-
     fn bounds(ctx: &Context) -> Rect {
         let width = ctx.conf.window_mode.width;
         let height = ctx.conf.window_mode.height;
