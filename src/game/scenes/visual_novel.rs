1462df43829316654b63c454552988c8cc84cecd
Nicholas Farley <nicholasafarley@gmail.com>
2018-04-08 13:15:10 -0400

    Reorder game/scenes/visual_novel

--- src/game/scenes/visual_novel.rs
+++ src/game/scenes/visual_novel.rs
@@ -3,54 +3,6 @@ use ggez::{self, GameResult, graphics::{self, BlendMode, DrawMode, DrawParam, Dr
 use engine::{self, color, draw_cache::{DrawCache, TryIntoDrawable}, ui::Message,
              visual_novel::command::{BackgroundCommand, Command}};
 
-pub enum Status {
-    CommandsApplied,
-    PendingCommands,
-}
-
-enum Background {
-    Color(graphics::Color),
-}
-
-impl TryIntoDrawable<BackgroundCache> for Background {
-    fn try_into_drawable(&self, ctx: &mut ggez::Context) -> GameResult<BackgroundCache> {
-        let h = ctx.conf.window_mode.height as f32;
-        let w = ctx.conf.window_mode.width as f32;
-        let points = [
-            Point2::new(0.0, 0.0),
-            Point2::new(0.0, h),
-            Point2::new(w, h),
-            Point2::new(w, 0.0),
-        ];
-        let mesh = graphics::Mesh::new_polygon(ctx, DrawMode::Fill, &points)?;
-        Ok(BackgroundCache::Mesh(mesh))
-    }
-}
-
-enum BackgroundCache {
-    Mesh(graphics::Mesh),
-}
-
-impl Drawable for BackgroundCache {
-    fn draw_ex(&self, ctx: &mut ggez::Context, mode: DrawParam) -> GameResult<()> {
-        match self {
-            &BackgroundCache::Mesh(ref mesh) => mesh.draw_ex(ctx, mode),
-        }
-    }
-
-    fn set_blend_mode(&mut self, mode: Option<BlendMode>) {
-        match self {
-            &mut BackgroundCache::Mesh(ref mut mesh) => mesh.set_blend_mode(mode),
-        }
-    }
-
-    fn get_blend_mode(&self) -> Option<BlendMode> {
-        match self {
-            &BackgroundCache::Mesh(ref mesh) => mesh.get_blend_mode(),
-        }
-    }
-}
-
 pub struct VisualNovel {
     commands: Vec<Command>,
     command_index: usize,
@@ -112,5 +64,53 @@ impl<I, F> engine::scene::Scene<I, F> for VisualNovel {
             message.draw_ex(ctx, DrawParam::default())?;
         }
         Ok(())
+    }
+}
+
+pub enum Status {
+    CommandsApplied,
+    PendingCommands,
+}
+
+enum Background {
+    Color(graphics::Color),
+}
+
+impl TryIntoDrawable<BackgroundCache> for Background {
+    fn try_into_drawable(&self, ctx: &mut ggez::Context) -> GameResult<BackgroundCache> {
+        let h = ctx.conf.window_mode.height as f32;
+        let w = ctx.conf.window_mode.width as f32;
+        let points = [
+            Point2::new(0.0, 0.0),
+            Point2::new(0.0, h),
+            Point2::new(w, h),
+            Point2::new(w, 0.0),
+        ];
+        let mesh = graphics::Mesh::new_polygon(ctx, DrawMode::Fill, &points)?;
+        Ok(BackgroundCache::Mesh(mesh))
+    }
+}
+
+enum BackgroundCache {
+    Mesh(graphics::Mesh),
+}
+
+impl Drawable for BackgroundCache {
+    fn draw_ex(&self, ctx: &mut ggez::Context, mode: DrawParam) -> GameResult<()> {
+        match self {
+            &BackgroundCache::Mesh(ref mesh) => mesh.draw_ex(ctx, mode),
+        }
+    }
+
+    fn set_blend_mode(&mut self, mode: Option<BlendMode>) {
+        match self {
+            &mut BackgroundCache::Mesh(ref mut mesh) => mesh.set_blend_mode(mode),
+        }
+    }
+
+    fn get_blend_mode(&self) -> Option<BlendMode> {
+        match self {
+            &BackgroundCache::Mesh(ref mesh) => mesh.get_blend_mode(),
+        }
     }
 }
