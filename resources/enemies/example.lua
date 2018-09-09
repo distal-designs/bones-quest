7284e0c9318dae87193ec9e7ccabf80ca25895cf
Nicholas Farley <nicholasafarley@gmail.com>
2018-09-09 17:30:35 -0400

    Add enemy example definition file

---
+++ resources/enemies/example.lua
@@ -0,0 +1,36 @@
+return {
+  name = "Example",
+  id = "example",
+  fights = "BONES",
+
+  default_state = "IDLE",
+  states = {
+
+    IDLE = {
+      frames = 10,
+      vulnerability = {
+        left = "WHIFF",
+        right = "BLOCK",
+        parry = false,
+      },
+      hitzones = {
+        left = false,
+        right = false,
+        duck = false,
+        stand = false,
+      }
+      on_hitting_player = "IDLE",
+      on_getting_hit = "IDLE",
+      on_block = "IDLE",
+      on_parry = "IDLE",
+      on_end = function (world)
+        if math.random(100) > 30 then
+          return "IDLE"
+        else
+          return "IDLE"
+        end
+      end
+    }
+
+  }
+}
