4a028b2e46b93a3212e8ac75f48e432c5f1fdbea
Nicholas Farley <nicholasafarley@gmail.com>
2018-09-10 19:38:46 -0400

    Add a small section in main testing lua code loading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate ggez;
+extern crate rlua;
 extern crate queues;
 extern crate regex;
 #[macro_use]
@@ -7,6 +8,8 @@ extern crate serde_derive;
 
 mod engine;
 
+
+use std::io::Read;
 
 use ggez::{conf, event, Context};
 
@@ -19,6 +22,16 @@ fn main() {
     let mut c = conf::Conf::default();
     c.window_setup.title = "Bones Quest".to_string();
     let ctx = &mut Context::load_from_conf("bones-quest", "distal-designs", c).unwrap();
+
+    let lua = rlua::Lua::new();
+    let mut script = String::new();
+    ctx.filesystem
+        .open("/enemies/example.lua")
+        .unwrap()
+        .read_to_string(&mut script)
+        .unwrap();
+    let enemy_table: rlua::Table = lua.exec(&script, None).unwrap();
+    println!("{:?}", enemy_table);
 
     let state = &mut MainState::new();
     let dialog = Command::load(&mut ctx.filesystem, "/dialogs/blood.toml").unwrap();
