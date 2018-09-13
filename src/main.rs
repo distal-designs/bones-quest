e3281510bca2515af9840c994114aec39af2abad
Nicholas Farley <nicholasafarley@gmail.com>
2018-09-12 23:45:28 -0400

    Modify working example of lua script to show require

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use ggez::{conf, event, Context};
 use engine::main_state::MainState;
 use engine::scenes::visual_novel::VisualNovel;
 use engine::visual_novel::command::Command;
+use engine::lua::LuaExt;
 
 
 fn main() {
@@ -23,15 +24,20 @@ fn main() {
     c.window_setup.title = "Bones Quest".to_string();
     let ctx = &mut Context::load_from_conf("bones-quest", "distal-designs", c).unwrap();
 
-    let lua = rlua::Lua::new();
+    let lua = rlua::Lua::new_with_path();
     let mut script = String::new();
     ctx.filesystem
         .open("/enemies/example.lua")
         .unwrap()
         .read_to_string(&mut script)
         .unwrap();
-    let enemy_table: rlua::Table = lua.exec(&script, None).unwrap();
-    println!("{:?}", enemy_table);
+    let script = r#"
+    local enemy = require 'resources.enemies.example'
+    for i,v in pairs(enemy) do
+      print(i,v)
+    end
+    "#;
+    lua.eval::<rlua::Value>(&script, None).unwrap();
 
     let state = &mut MainState::new();
     let dialog = Command::load(&mut ctx.filesystem, "/dialogs/blood.toml").unwrap();
