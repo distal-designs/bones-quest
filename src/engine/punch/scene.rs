97b8149a9bc8e59de481a0dde08802b30bd425df
Nicholas Farley <nicholasafarley@gmail.com>
2018-09-30 20:40:10 -0400

    Move engine::scenes::_Fight -> punch::scene::Scene

---
+++ src/engine/punch/scene.rs
@@ -0,0 +1,18 @@
+use ggez::{self, GameResult};
+
+use engine;
+
+
+pub struct Scene;
+
+
+impl<I, F> engine::scene::Scene<I, F> for Scene {
+    fn update(&mut self, _: &I, _: &mut F) -> GameResult<()> {
+        unimplemented!();
+    }
+
+
+    fn draw(&self, _: &F, _ctx: &mut ggez::Context) -> GameResult<()> {
+        unimplemented!();
+    }
+}
