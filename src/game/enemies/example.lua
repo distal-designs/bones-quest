3c0a88b2cdb92de5fe9937aa14c8d759a8d4a41d
Nicholas Farley <nicholasafarley@gmail.com>
2018-11-17 00:32:28 -0500

    Add a PUNCHED state to example enemy

--- src/game/enemies/example.lua
+++ src/game/enemies/example.lua
@@ -9,8 +9,48 @@ return {
     IDLE = {
       frames = 120,
       vulnerability = {
-        left = "WHIFF",
+        left = "HIT",
         right = "BLOCK",
+        parry = false,
+      },
+      hitzones = {
+        left = false,
+        right = false,
+        duck = false,
+        stand = false,
+      },
+      after_hitting_player = "IDLE",
+      on_getting_hit = "PUNCHED",
+      on_block = "IDLE",
+      on_parry = "IDLE",
+      on_end = "ATTACK",
+    },
+
+    ATTACK = {
+      frames = 10,
+      vulnerability = {
+        left = "HIT",
+        right = "BLOCK",
+        parry = false,
+      },
+      hitzones = {
+        left = true,
+        right = false,
+        duck = false,
+        stand = false,
+      },
+      after_hitting_player = "IDLE",
+      on_getting_hit = "PUNCHED",
+      on_block = "IDLE",
+      on_parry = "IDLE",
+      on_end = "IDLE",
+    },
+
+    PUNCHED = {
+      frames = 30,
+      vulnerability = {
+        left = "WHIFF",
+        right = "WHIFF",
         parry = false,
       },
       hitzones = {
@@ -23,28 +63,8 @@ return {
       on_getting_hit = "IDLE",
       on_block = "IDLE",
       on_parry = "IDLE",
-      on_end = "ATTACK",
-    },
-
-    ATTACK = {
-      frames = 10,
-      vulnerability = {
-        left = "WHIFF",
-        right = "BLOCK",
-        parry = false,
-      },
-      hitzones = {
-        left = true,
-        right = false,
-        duck = false,
-        stand = false,
-      },
-      after_hitting_player = "IDLE",
-      on_getting_hit = "IDLE",
-      on_block = "IDLE",
-      on_parry = "IDLE",
       on_end = "IDLE",
-    }
+    },
 
   }
 }
