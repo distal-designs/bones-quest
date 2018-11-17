cf83272ecfd871636b7215860048651ff26df04e
Nicholas Farley <nicholasafarley@gmail.com>
2018-11-16 21:23:28 -0500

    Add ATTACK state to enemy example

--- resources/enemies/example.lua
+++ resources/enemies/example.lua
@@ -30,6 +30,26 @@ return {
           return "IDLE"
         end
       end
+    },
+
+    ATTACK = {
+      frames = 10,
+      vulnerability = {
+        left = "WHIFF",
+        right = "BLOCK",
+        parry = false,
+      },
+      hitzones = {
+        left = true,
+        right = false,
+        duck = false,
+        stand = false,
+      },
+      after_hitting_player = "IDLE",
+      on_getting_hit = "IDLE",
+      on_block = "IDLE",
+      on_parry = "IDLE",
+      on_end = "IDLE",
     }
 
   }
